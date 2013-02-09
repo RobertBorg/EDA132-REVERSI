032f59a581fb135fa32db6dc384ef2ceaf88c722
Robert Borg <rauban@gmail.com>
2013-02-09 18:25:47 +0100

    now correctly calculates possible moves

--- src/board_position.h
+++ src/board_position.h
@@ -2,7 +2,11 @@
 #define BOARD_POSITION_H__
 
 #include <cstdint>
+#include <iostream>
 #include "board_direction.h"
+
+using std::cout;
+using std::endl;
 
 template<uint_fast8_t width = 8, uint_fast8_t height = 8 >
 struct BoardPosition {
@@ -26,8 +30,12 @@ struct BoardPosition {
 		return *this;
 	}
 
-	bool isValid() {
+	bool isValid() const {
 		return x < width && y < height;
+	}
+
+	void print() const {
+		cout << static_cast<char>('a' + x) << y+1 << endl;
 	}
 };
 
